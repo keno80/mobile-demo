70d2974b3db97289317e14f590dd589c7dd9fa2d
keno80 <1211582528@qq.com>
2020-08-01 19:11:21 +0800

    调整内容显示区域

--- src/overlay/components/AppMain.vue
+++ src/overlay/components/AppMain.vue
@@ -16,5 +16,14 @@
 </script>
 
 <style scoped>
-
+  .app-main {
+    /*50 = navbar  */
+    min-height: calc(100vh - 100px);
+    width: 93.7%;
+    position: relative;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    min-width: 100px;
+    padding: 10px 10px 0 10px;
+  }
 </style>
